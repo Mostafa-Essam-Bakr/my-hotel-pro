8a24b49593f556fd59952c9193196935d0514b46
Mostafa-Essam-Bakr <elturkeym0@gmail.com>
2023-12-29 21:21:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import SignUp from "./Components/Sign up"
+import Login from "./Components/Login"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./Components/Home.css"
+
+import { Routes,Route } from "react-router-dom"
+import Dashbord from "./Dashbord"
+import Home from "./Home"
+import Users from "./Components/Users"
+import Updateuser from "./Updateuser"
+import Create from "./Create"
+import Setings from "./Setings"
+import Book from "./Book"
+import Hotel from "./Hotel";
+
+
+export default function App(){
+  return ( <div>
+<Routes>
+
+<Route path="/" element={<Home/>}/>
+  <Route path="/Register" element={<SignUp/>}/>
+
+  <Route path="/Login" element={<Login/>}/>
+
+
+
+
+
+
+
+
+
+  <Route path="/Dashbord" element={<Dashbord/>}>
+<Route path="Users" element={<Users/>}/>
+<Route path="Create" element={<Create/>}/>
+<Route path="Setings" element={<Setings/>}/>
+<Route path="Users/:id" element={<Updateuser/>}/>
+ </Route>
+
+
+  <Route path="Hotel" element={<Hotel/>}/>
+
+
+
+  <Route path="/Book" element={<Book/>}/>
+
+
+
+
+
+
+
+
+
+
+
+</Routes>
+  </div>)
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*                                        صفحه جديد بأسم Alldata               
+export function Data(props){
+  return <div><h1>{props.name}</h1></div>
+}
+*/
+
+
+/*                                              صفحه تابعه ل Alldata 
+                            تستخدم هذه الصفح ل جلب بيانات من خلال الباك اند داخل الموقع
+
+import  { useEffect, useState } from "react";
+import { Data } from "./Components/Alldata";
+
+export default function App(){
+
+  const[data,setData]=useState([])
+
+const dataShow= data.map((e,index)=> <Data name={e} key={index} />)
+
+
+useEffect(()=>{
+  fetch("https://www.balldontlie.io/api/v1/teams")
+  .then((res)=> res.json())
+  .then((data) =>setData(data.data.map((e)=> e.name)))
+},[])
+
+
+
+return(<div>{dataShow}</div> )
+
+}
+*/
